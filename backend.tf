cc4ac1495cc10d94e2911a0596c9e2e6dc88f6e3
Reginald <info.whitecranberry@gmail.com>
2024-08-02 06:00:12 -0500

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-rb-terraform-state" #replace with your bucket
+    bucket         = "dfjalifjli" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "idjfa;oidfja;" #replace with the table
   }
 }
